b24d8671b25fcc3f8e4718b68500d2a3845bb220
Haans Lopez <jesuslopez@ciencias.unam.mx>
2023-10-17 00:27:41 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+// Author: Jesus Haans Lopez Hernandez - jesuslopez@ciencias.unam.mx
+const http = require('http')
+const server = http.createServer((peticion, respuesta) => {
+  respuesta.writeHead(200,{'Content-Type':'text/plain'})
+  respuesta.end("Hola mundo")
+})
+server.listen(3000)
